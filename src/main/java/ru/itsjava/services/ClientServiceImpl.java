a16d04188c448b19c425cb3e88f4e17d837325da
YakovBSergeev <154790912+YakovBSergeev@users.noreply.github.com>
2024-03-22 15:36:45 +0600

    HW-424.Client

--- src/main/java/ru/itsjava/services/ClientServiceImpl.java
+++ src/main/java/ru/itsjava/services/ClientServiceImpl.java
@@ -2,29 +2,39 @@ package ru.itsjava.services;
 
 import lombok.SneakyThrows;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class ClientServiceImpl implements ClientService{
+public class ClientServiceImpl implements ClientService {
     public final static int PORT = 8081;
     public final static String HOST = "localhost";
+
     @SneakyThrows
     @Override
     public void start() {
         Socket socket = new Socket( HOST, PORT );
         if (socket.isConnected()) {
-            new Thread(new SocketRunnable( socket )).start();
+            new Thread( new SocketRunnable( socket ) ).start();
 
             PrintWriter serverWriter = new PrintWriter( socket.getOutputStream() );
             MessageInputService messageInputService =
                     new MessageInputServiceImpl( System.in );
-            System.out.println( "Введите сообщение" );
-            String consoleMessage = messageInputService.getMessage();
-            serverWriter.println(consoleMessage);
+
+            System.out.println( "Введите свой логин:" );
+            String login = messageInputService.getMessage();
+
+            System.out.println( "Введите свой пароль:" );
+            String password = messageInputService.getMessage();
+
+            serverWriter.println( "!autho!" + login + ":" + password );
             serverWriter.flush();
 
+            while (true) {
+//                System.out.println( "Введите сообщение" );
+                String consoleMessage = messageInputService.getMessage();
+                serverWriter.println( consoleMessage );
+                serverWriter.flush();
+            }
 
         }
 
